48a922396e0cb0164c6b1e4f36329a86601b63f5
NaufalMahdi <71993847+NaufalMahdi@users.noreply.github.com>
2022-11-03 17:57:36 +0700

    Update post.blade.php

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -7,7 +7,13 @@
 <p>Kategori : <a href="/kategories">{{ $post->kategori->name }} </a></p>
 
 {!! $post->body !!}
-
+<table class="table table-bordered">
+					<tr>
+						<th>NO</th>
+                        <th>Kecamatan</th>
+						<th>Jumlah</th>
+						
+					</tr>
 
 <a href="/posts">Back to Posts</a>
 @endsection 
